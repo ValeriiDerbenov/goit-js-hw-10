c290dec3782dd4510320ffe94cef6521f9af1703
Valerii <vd.alternative@gmail.com>
2023-11-16 14:16:00 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+
+new SlimSelect({
+  select: '#selectElement',
+});
+
 const elements = {
   selectEl: document.querySelector('.breed-select'),
   textMarkEl: document.querySelector('.cat-info'),
