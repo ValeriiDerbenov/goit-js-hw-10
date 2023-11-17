80a056f3c3ecad6b8528589f5811d69246886475
Valerii <vd.alternative@gmail.com>
2023-11-18 00:43:24 +0200

    1234

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,9 @@
+import axios from 'axios';
 const URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_cFPQrAg5CYBZmRDtRfaA24RnDxTxorzx1kegFGzoVb7BNT7IjU552Hsjjqd8xIoO';
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+import axios from 'axios';
 
 export function fetchBreeds() {
   return fetch(`${URL}/breeds?api_key=${API_KEY}`).then(response => {
